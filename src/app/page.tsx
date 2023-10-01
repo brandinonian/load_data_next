8476a8866decaf2bf52f30baf2024c2dab2cb73b
Brandon Phillips <14bphillips@gmail.com>
2023-10-01 09:53:45 -0600

    Building pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
 
   return (
     <div className="bg-slate-800 pt-30">
-      <div className="bg-slate-600 py-20">
-        <h1>Home</h1>
+      <div className="bg-slate-600 py-20 px-40 flex flex-col items-center">
+        <Link href="/referenceloads" className="m-5 btn btn-block text-xl">Reference Loads</Link>
+        <Link href="/factoryloads" className="m-5 btn btn-block text-xl">Factory Loads</Link>
+        <Link href="/customloads" className="m-5 btn btn-block text-xl">Custom Loads</Link>
       </div>
     </div>
   )
